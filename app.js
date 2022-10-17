0340ec2c70aeea080037c81de1be9a1ddc363bc4
calebtripp <calebtripp@outlook.com>
2022-10-17 17:21:41 -0400

    Exercises 1 and 2 complete

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+//let i = 0;
+for (let i = 1; i <= 100; i++)
+{
+ if    (i % 2 != 0)
+{ 
+    console.log(" "+i+" is an odd number.");
+}
+ else continue;
+}
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i <= 100; i++)
+{
+ if    (i % 5 == 0 && i % 3 == 0)
+{ 
+    console.log("FIZZBUZZ");
+}
+else if    (i % 5 == 0)
+{ 
+    console.log("BUZZ");
+}
+else if    (i % 3 == 0)
+ { 
+     console.log("FIZZ");
+ }
+ else continue;
+}
+
+//Exercise 3 Section
